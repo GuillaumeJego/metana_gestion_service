112731c385a5ada2c02d2c07f803641b90f47bf6
Diego <jego.guillaume22@gmail.com>
2024-04-25 14:48:18 +0200

    Mise en place base application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './features/components/header/header.component';
+import { FooterComponent } from './features/components/footer/footer.component';
+import { MainComponent } from './features/components/main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
   title = 'metana_gestion_finances';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';;
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './features/components/dashboard/dashboard.component';
+import { PrestationsComponent } from './features/components/prestations/prestations.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    {path: 'dashboard', component: DashboardComponent },
+    {path: 'prestations', component: PrestationsComponent},
+];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
